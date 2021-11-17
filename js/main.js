16f5efbfd67946fd311bf122857501243859fa42
Dans7an <kdanstanmark@gmail.com>
2021-11-17 13:20:50 -0500

    added unsplash

--- js/main.js
+++ js/main.js
@@ -17,11 +17,13 @@ let title = document.querySelector('h2')
 // DEFAULT PHOTO
 let defaultPhoto = "https://cavchronicle.org/wp-content/uploads/2018/03/top-travel-destination-for-visas-900x504.jpg"
 
+// VARIABLES FOR API
+const apiKey = 'wQuC3g7JmlN_8yTfq4Hk9247zZRTDzpaZSQyE1b32bE' 
+
 function submitForm(){
     //  CAPTURES ALL THE TEXT FIELDS
     let name = inputs[0].value;
     let location = inputs[1].value;
-    let photo = inputs[2].value;
     let desciption = document.querySelector('textarea').value; 
 
     // INSERT INFO INTO CARD
@@ -30,7 +32,8 @@ function submitForm(){
         document.querySelector('.card').classList.remove('makeCardVisible')
         cardName.innerText = name;
         cardLocation.innerText = location;
-        photo.length > 1 ? cardPhoto.setAttribute('src',photo) : cardPhoto.setAttribute('src',defaultPhoto);
+        addImageFromAPI(location)
+        // photo.length > 1 ? cardPhoto.setAttribute('src',photo) : cardPhoto.setAttribute('src',defaultPhoto);
         cardDescription.value = desciption;    
     }
 
@@ -60,3 +63,22 @@ function removeCard(){
     title.innerText = "Enter destination details"
     document.querySelector('.card').classList.add('makeCardVisible')
 }
+
+function addImageFromAPI(apiLocation){
+    fetch(`https://api.unsplash.com/search/photos?query=${apiLocation}&client_id=${apiKey}`)
+    .then(res => res.json())
+    .then(data => {
+        const arraySize = data.results.length
+        const randomNumber = Math.floor(Math.random() * arraySize)
+        const imageUrl = data.results[randomNumber].urls.raw
+        console.log(imageUrl)
+        cardPhoto.setAttribute('src',imageUrl)
+    }
+        )
+    // photo.length > 1 ? cardPhoto.setAttribute('src',photo) : cardPhoto.setAttribute('src',defaultPhoto);
+
+}
+
+
+// wQuC3g7JmlN_8yTfq4Hk9247zZRTDzpaZSQyE1b32bE    Access Key
+// HaZutscWaJSJN6hhCHmBIv_ADIKupyVB3NT5aeqKmvE      Secret key
